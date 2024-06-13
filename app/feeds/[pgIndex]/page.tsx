b540ba3635b71a217042b36ef955e8eddb6a8f06
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-13 10:20:25 +0800

    :bee: Clear default page and add demo to feeds.

--- app/feeds/[pgIndex]/page.tsx
+++ app/feeds/[pgIndex]/page.tsx
@@ -5,11 +5,16 @@ import { BASE_FILES } from '@/lib/db_config';
 import { Grid } from '@/components/Layout';
 import { Feed } from '@/components/uiComponents/Feed';
 
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from 'next-auth/next';
+
 export default async function Home({ params }: any) {
+    const session = await getServerSession(options);
 
     let feedsLR = await FetchFeeds();
     return (
         <Main title={"Feeds"}>
+            {session ? (<h1>{session?.user?.name}</h1>) : (<h1>Not logged in.</h1>)}
             <Grid>
                 {feedsLR?.items.map((item, index) => (
                     <Feed key={index} index={index} item={item}></Feed>
